7df4a7057c6fcab15b09edfbf7086786269a1f18
Bezyl Mophat Otieno <bezyl.otieno@griffinglobaltech.com>
2024-04-21 12:07:22 +0300

    Use clerk auth for the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,21 @@
 import Link from "next/link";
 import Image from "next/image";
 import { db } from "~/server/db";
+// import { SignedOut } from '@clerk/nextjs';
+// import { SignedIn } from '@clerk/nextjs';
 
 // making the page dynamic from next js
 // export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
 
+  export async  function RenderedImages () {
+    
 
   const images = await db.query.images.findMany({
     orderBy: (model, order) => order.asc(model.id),
   });
-
-
-
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#050505] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-10 px-4 py-16 ">
-        <h1 className="text-4xl font-bold">Welcome to T3 Gallery</h1>
-        <p className="text-lg">This is a simple image gallery</p>
-        <p className="text-lg">Click on an image to view it</p>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1">
+    return (
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1">
           {[...images,...images, ...images].flat().map((image) => (
             <Link key={image.id} href={`/image/${image.id}`}>
               <div className="relative w-full h-64 md:h-80 lg:h-96">
@@ -38,6 +31,25 @@ export default async function HomePage() {
             </Link>
           ))}
         </div>
+    )
+  }
+
+export default async function HomePage() {
+
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#050505] to-[#15162c] text-white">
+      <div className="container flex flex-col items-center justify-center gap-10 px-4 py-16 ">
+        <h1 className="text-4xl font-bold">Welcome to T3 Gallery</h1>
+        {/* <SignedOut> */}
+          <div className="flex align-top justify-between" >
+            Please sign in above
+          </div>
+        {/* <SignedOut/> */}
+        {/* <SignedIn> */}
+          <RenderedImages/>
+        {/* <SignedIn/> */}
+  
        </div>
     </main>
   );
